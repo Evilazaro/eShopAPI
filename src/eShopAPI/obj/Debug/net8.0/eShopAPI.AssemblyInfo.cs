e7cd8c85d7f9cafed8bcb82d4243569afc83ff95
Evilázaro Alves <evilazaro@gmail.com>
2023-12-18 07:45:06 -0600

    Removing Unit Tests

--- src/eShopAPI/obj/Debug/net8.0/eShopAPI.AssemblyInfo.cs
+++ src/eShopAPI/obj/Debug/net8.0/eShopAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eShopAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c080101abe9a5bd08ba14359fc04303d950a6ddf")]
 [assembly: System.Reflection.AssemblyProductAttribute("eShopAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eShopAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
